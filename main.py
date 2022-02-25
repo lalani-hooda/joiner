42132d4b3eb9d90eb5cc0ab28f72e920c37dab6c
Zeeshan Hooda <zeeshan.hooda@smartaccess.io>
2022-02-24 19:52:33 -0700

    added basic file type handling and reading

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import argparse
-from os.path import exists
+from enum import Enum, auto
+import io
+import pandas as pd
+import pathlib
+
+class DataType(Enum):
+    CSV = auto()
+    EXCEL = auto()
+
 
 def _dump_args(args: dict):
     t = vars(args)
     print('DESCRIPTION')
     [print(f'\t{k:10}\t{t[k]}') for _, k in enumerate(t)]
+
+
+def _get_data_type(file_path: str) -> DataType:
+    ext = pathlib.Path(file_path).suffix
+    if ext == '.csv':
+        return DataType.CSV
+    if ext in ['.xls', '.xlsx']:
+        return DataType.EXCEL
+
+
+def _read_file(file: io.TextIOWrapper) -> pd.DataFrame:
+    dtype = _get_data_type(file.name)
+    if dtype == DataType.CSV:
+        return pd.read_csv(file)
+    if dtype == DataType.EXCEL:
+        return pd.read_excel(file)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='a small command line tool to join files')
@@ -20,7 +44,14 @@ if __name__ == "__main__":
     if args.right_key is None:
         args.right_key = args.left_key
     
-    
     _dump_args(args)
     
+    left_df = _read_file(args.left)
+    right_df = _read_file(args.right)
+    
+    print(left_df[args.left_key].head())
+    print(right_df[args.right_key].head())
+    
+    args.left.close()
+    args.right.close()
     

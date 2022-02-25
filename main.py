f1d3b75c0e210559f5177ca16ec25d90906fca01
Zeeshan Hooda <zeeshan.hooda@smartaccess.io>
2022-02-24 20:43:37 -0700

    added dataframe merging and output file writing

--- main.py
+++ main.py
@@ -29,7 +29,16 @@ def _read_file(file: io.TextIOWrapper) -> pd.DataFrame:
         return pd.read_csv(file)
     if dtype == DataType.EXCEL:
         return pd.read_excel(file)
+    
 
+def _write_file(df: pd.DataFrame, file_path: str):
+    dtype = _get_data_type(file_path)
+    if dtype == DataType.CSV:
+        return df.to_csv(file_path, index=False)
+    if dtype == DataType.EXCEL:
+        return df.to_excel(file_path, index=False)
+
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='a small command line tool to join files')
     parser.add_argument('left', help='left table to join', type=argparse.FileType('r', encoding='utf8'))
@@ -37,7 +46,7 @@ if __name__ == "__main__":
     
     required_named = parser.add_argument_group('required named arguments')
     required_named.add_argument('-l', '--left-key', help='left field to join on', required=True)
-    parser.add_argument('-r', '--right-key', help='right field to join on (leave empty for natural join)', required=True)
+    parser.add_argument('-r', '--right-key', help='right field to join on (leave empty for natural join)', required=False)
     required_named.add_argument('-o', '--output', help='output file', required=True)
     args = parser.parse_args()
     
@@ -49,8 +58,10 @@ if __name__ == "__main__":
     left_df = _read_file(args.left)
     right_df = _read_file(args.right)
     
-    print(left_df[args.left_key].head())
-    print(right_df[args.right_key].head())
+    merged_df = left_df.merge(right_df, left_on=args.left_key, right_on=args.right_key, how='left')
+    print(merged_df[[args.left_key, args.right_key]].head())
+    
+    _write_file(merged_df, args.output)
     
     args.left.close()
     args.right.close()
